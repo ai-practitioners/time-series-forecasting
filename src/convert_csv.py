1a552606a92b25c848fc5e03f628ba7108b26ce9
ellacharmed <ellacharmed@gmail.com>
2023-07-02 13:59:44 +0800

    Add files for initial exploration, dataset swapped

--- src/convert_csv.py
+++ src/convert_csv.py
@@ -7,8 +7,8 @@ c = conn.cursor()
 calendar = pd.read_csv('data/raw/calendar.csv')
 calendar.to_sql('calendar', conn, if_exists='append', index = False, chunksize = 10000)
 
-train = pd.read_csv('data/raw/sales_train_evaluation.csv')
-train.to_sql('train', conn, if_exists='append', index = False, chunksize = 10000)
+sales_train = pd.read_csv('data/raw/sales_train_evaluation.csv')
+sales_train.to_sql('sales_train', conn, if_exists='append', index = False, chunksize = 10000)
 
-prices = pd.read_csv('data/raw/sell_prices.csv')
-prices.to_sql('prices', conn, if_exists='append', index = False, chunksize = 10000)
+sell_prices = pd.read_csv('data/raw/sell_prices.csv')
+sell_prices.to_sql('sell_prices', conn, if_exists='append', index = False, chunksize = 10000)
