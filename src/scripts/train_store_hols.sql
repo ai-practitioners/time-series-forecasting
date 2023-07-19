27dff5584d9d33f4e3b434afe6c1945170f613e3
ellacharmed <ellacharmed@gmail.com>
2023-07-19 11:53:29 +0800

    Add full-df as view and edit other with date parts

--- src/scripts/train_store_hols.sql
+++ src/scripts/train_store_hols.sql
@@ -4,15 +4,21 @@ select
 	locale, locale_name, description, transferred,
 	st.`type`, hols.`type` hol_type, 
 	tr.store_nbr,
-	tr.`date` 
+	tr.`date`, 
+	YEAR(tr.`date`) `year`,
+    MONTH(tr.`date`) `month`,
+    DAY(tr.`date`) `day_of_month`,
+	sum(transactions)
 from
 	train as tr
 left join stores as st
-on
-	tr.store_nbr = st.store_nbr
+    on tr.store_nbr = st.store_nbr
 left join holidays_events hols 
-on tr.`date` = hols.`date`
-where
-	year(tr.`date`) between 2013 and 2015
+    on tr.`date` = hols.`date`
+left join transactions txn 
+    on tr.`date` = txn.`date` AND tr.store_nbr = txn.store_nbr
+	
+-- where
+-- 	year(tr.`date`) between 2013 and 2015
 -- constraint where rowcount (1,972,674)
 -- no constraint, full rowcount (3,054,348)
