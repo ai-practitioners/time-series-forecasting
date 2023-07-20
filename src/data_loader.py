62bc0e010a88c0f74f91625bd7b2c74e4fd6a3ce
ellacharmed <ellacharmed@gmail.com>
2023-07-21 00:59:40 +0800

    apply-stash

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
 from dotenv import dotenv_values
-from sqlalchemy import create_engine
+import polars as pl
 import pandas as pd
+from sqlalchemy import create_engine
+import mysql.connector
+import MySQLdb
+import connectorx as cx
 
 from logger import logging
 
@@ -21,7 +25,7 @@ class CSVDataLoader(DataLoader):
     '''
     def load(self, file_path="data/raw/stores.csv"):
         print('Loading CSV data...')
-        return pd.read_csv(file_path)
+        return pl.read_csv(file_path)
 
 
 class DBDataLoader(DataLoader):
@@ -38,7 +42,13 @@ class DBDataLoader(DataLoader):
     def __init__(self):
         self.database = self._get_database_engine()
         logging.info("[+] Local Connection Successful")
-        self.query = 'SELECT * FROM stores'
+        
+        self.query = 'SELECT * FROM quito_frm_view'
+
+
+        # self.connection_uri=f'mysql+pymysql://{user}:{password}@{host}/{db}'
+        # self.connection_uri=f'mysql+mysqldb://{user}:{password}@{host}/{db}'
+        
 
 
     def load(self, query=None):
@@ -47,15 +57,39 @@ class DBDataLoader(DataLoader):
             query = self.query
         print('Loading dataset from database...')
         
-        return pd.read_sql(query, self.database, parse_dates='date', chunksize=10000)
-    # (query, self.database, index_col='date', parse_dates='date', chunksize=10000)
+        # using connectorx
+        return cx.read_sql(self.database, self.query )  
+        # pd.read_sql(query, self.database, parse_dates='date', chunksize=10000)
+        # pl.read_database(query=query, connection_uri=self.connection_uri)
+        # pd.read_sql(query, self.database, parse_dates='date', chunksize=10000)
 
 
     def _get_database_engine(self):
-        host = self.config.get('ENDPOINT')
-        port = int(self.config.get('PORT'))
+
+        # local DB
+        # port = int(self.config.get('PORT'))
         user = self.config.get('USERNAME')
         password = self.config.get('PASSWORD')
+        host = self.config.get('ENDPOINT')
         db = self.config.get('DBNAME')
 
-        return create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{db}')
+        # planetscale
+        # user = self.config.get("ps_username")
+        # password = self.config.get("ps_password")
+        # host = self.config.get("ps_host")
+        # db = self.config.get("ps_database")
+        
+        # using connectorx
+        return f'mysql://mysql+mysqldb://{user}:{password}@{host}/{db}'
+        # "mysql://{user}:{pw}@{host}:{port}/{db}".format(user=DB_USERNAME,
+                                                    #    pw=DB_PASSWORD,
+                                                    #    host=DB_HOST,
+                                                    #    db=DB_DATABASE,
+                                                    #    port=3306,
+                                                    #    ssl_verify_identity=True,
+                                                    #    ssl_ca="path/to/ssl_cert"
+                                                    #    )
+        # using mysql.connector
+        # return mysql.connector.connect(f'mysql+mysqldb://{user}:{password}@{host}/{db}')
+        # using sqlalchemy
+        # return create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{db}')
