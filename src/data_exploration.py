964ce1470425cbb2ff4d9c1a27bed2ba7fbeec29
ggjx22 <navlag18011@gmail.com>
2023-09-16 17:04:55 +0800

    added new func plot_sales_averages

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,6 +1,10 @@
 from geopy import geocoders
 from operator import itemgetter
 from logger import logging
+import helper as helper
+import polars as pl
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def get_geo_coordinates(username, location_name, country_code_iso):
     '''
@@ -63,3 +67,212 @@ def get_geo_coordinates(username, location_name, country_code_iso):
     except Exception as e:
         print(e)
         return None
+    
+def plot_sales_averages(dataframe, select_hierarchy, name):
+    
+    # create an aggregated dataframe based on user selection of either state or city
+    agg_df = helper.get_aggregate_data(dataframe=dataframe, select_hierarchy=select_hierarchy)
+    
+    # check if date column is datetime datatype and sorted 
+    if agg_df['date'].dtype != pl.Datetime:
+        agg_df = agg_df.with_columns(pl.col('date').cast(pl.Datetime))
+    else:
+        pass
+    
+    # further subset the dataframe based on user selection of name of state or city
+    sub_df = agg_df.filter(pl.col(select_hierarchy) == name)
+    
+    # create temporal averages
+    sub_avg_df = (
+        sub_df
+        .with_columns(
+            weekly_avg=pl.col('sales').rolling_mean(window_size='1w', by='date', closed='left')
+        )
+        .with_columns(
+            monthly_avg=pl.col('sales').rolling_mean(window_size='1mo_saturating', by='date', closed='left')
+        )
+        .with_columns(
+            quarterly_avg=pl.col('sales').rolling_mean(window_size='1q_saturating', by='date', closed='left')
+        )
+        .with_columns(
+            yearly_avg=pl.col('sales').rolling_mean(window_size='1y_saturating', by='date', closed='left')
+        )    
+    )
+    
+    # create a figure with two subplots
+    sales_fig = make_subplots(
+        rows=2,
+        cols=1,
+        subplot_titles=(
+            'Actual vs Weekly/Monthly Avg',
+            'Actual vs Quaterly/Yearly Avg',
+        )
+    )
+
+    # add actual trace to the top subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['sales'],
+            name='Actual Sales (Weekly/Monthly)',
+            line=dict(color='cyan'),
+            legendgroup='group1',
+            hovertemplate='Actual Sales (Weekly/Monthly): %{y}'
+        ),
+        row=1, col=1,
+    )
+
+    # add weekly avg trace to the top subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['weekly_avg'],
+            name='Weekly Sales',
+            line=dict(color='magenta'),
+            legendgroup='group1',
+            hovertemplate='Weekly Sales: %{y}'
+        ),
+        row=1, col=1
+        
+    )
+
+    # add monthly trace to the top subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['monthly_avg'],
+            name='Monthly Sales',
+            line=dict(color='yellow'),
+            legendgroup='group1',
+            hovertemplate='Monthly Sales: %{y}'
+        ),
+        row=1, col=1
+    )
+
+    # add actual trace again to the bottom subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['sales'],
+            name='Actual Sales (Quarterly/Yearly)',
+            line=dict(color='cyan'),
+            legendgroup='group2',
+            hovertemplate='Actual Sales (Quarterly/Yearly): %{y}'
+        ),
+        row=2, col=1,
+    )
+
+    # add quarterly avg trace to the bottom subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['quarterly_avg'],
+            name='Quaterly Sales',
+            line=dict(color='orange'),
+            legendgroup='group2',
+            hovertemplate='Quaterly Sales: %{y}'
+        ),
+        row=2, col=1
+    )
+
+    # add yearly avg trace to the bottom subplot
+    sales_fig.add_trace(
+        go.Scatter(
+            x=sub_avg_df['date'],
+            y=sub_avg_df['yearly_avg'],
+            name='Yearly Sales',
+            line=dict(color='lime'),
+            legendgroup='group2',
+            hovertemplate='Yearly Sales: %{y}'
+        ),
+        row=2, col=1
+    )
+
+    # general customisation of the figure
+    sales_fig.update_layout(
+        title=f'Temporal Averages Sales by State<br>State: {sub_avg_df.item(row=0, column="state")}',
+        showlegend=False,
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+        plot_bgcolor='Black',
+        paper_bgcolor='Black',
+        font=dict(
+            color='White',
+            size=12
+        ),
+        yaxis=dict(
+            title='Value',
+            tickprefix='$',
+            showgrid=True,
+            gridcolor='Darkgrey'
+        ),
+        xaxis=dict(
+            title='Date',
+            # tickangle=-20,
+            dtick='M1',
+            tickformat='%b\n%Y'
+        ),
+        autosize=True,
+        height=900
+    )
+
+    # give x-axes a proper month format
+    sales_fig.update_xaxes(
+        title_text='Date',
+        dtick='M1',
+        tickformat='%b\n%Y',
+        showgrid=False,
+        row=2, col=1
+    )
+
+    # give y-axes a proper dollar format
+    sales_fig.update_yaxes(
+        title_text='Value',
+        tickprefix='$',
+        row=2, col=1
+    )
+
+    # manually add legends
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.4, y=1,
+        text="<span style='font-size:12px;color:cyan'>●</span> Actual Sales",
+        showarrow=False
+    )
+
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.5, y=1,
+        text="<span style='font-size:12px;color:magenta'>●</span> Weekly Avg Sales",
+        showarrow=False
+    )
+
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.61, y=1,
+        text="<span style='font-size:12px;color:yellow'>●</span> Monthly Avg Sales",
+        showarrow=False
+    )
+
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.4, y=0.36,
+        text="<span style='font-size:12px;color:cyan'>●</span> Actual Sales",
+        showarrow=False
+    )
+
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.5, y=0.36,
+        text="<span style='font-size:12px;color:orange'>●</span> Quarterly Avg Sales",
+        showarrow=False
+    )
+
+    sales_fig.add_annotation(
+        xref="paper", yref="paper",
+        x=0.61, y=0.36,
+        text="<span style='font-size:12px;color:lime'>●</span> Yearly Avg Sales",
+        showarrow=False
+    )
+
+    return sales_fig.show()
